7d3bd00e950020958b5cb66704029f51278f6b9b
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2019-10-20 15:49:00 +0200

    isGoal implemented

--- src/main/java/StateSpace.java
+++ src/main/java/StateSpace.java
@@ -5,7 +5,43 @@ public class StateSpace {
 		
 	}
 	
-	public static void Succesors(String aaci ) {
+	public static void Succesors(Cube state ) {
 		
+		
+		//Devuelve [(acc1,NewState1,costAct1)],...,[(accN,NewStateN,costActN)]
 	}
+	
+	public static boolean isGoal(Cube state) {
+		boolean[] bolA = new boolean[6];
+		boolean bol = true;
+		
+		bolA[0] = comprobar_color_cara(state.getBack());
+		bolA[1] = comprobar_color_cara(state.getDown());
+		bolA[2] = comprobar_color_cara(state.getFront());
+		bolA[3] = comprobar_color_cara(state.getLeft());
+		bolA[4] = comprobar_color_cara(state.getRight());
+		bolA[5] = comprobar_color_cara(state.getUp());
+		
+		
+		for(int i= 0; i < bolA.length; i++) {
+			if(bolA[i]==false) {
+				bol = false;
+			}
+		}
+		return bol;
+	}
+	
+	public static boolean comprobar_color_cara(int[][] cara) {
+		int x = cara[0][0];
+		boolean bol = true;
+		for (int i = 0; i < cara.length;i++) {
+			for(int j = 0; j < cara.length;j++) {
+				if(x!=cara[i][j]) {
+					bol = false;
+				}
+			}
+		}
+		return bol;
+	}
+	
 }
